9322fa2b8590df35a76c9114968db2faff3e1038
vandat0528511-cyber <vandat0528511@gmail.com>
2026-07-23 08:37:49 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function upDate(previewPic) {
   console.log("Sự kiện upDate đã được kích hoạt thành công!");
-
   console.log("Alt text:", previewPic.alt);
   console.log("Source URL:", previewPic.src);
 
@@ -9,9 +8,8 @@ function upDate(previewPic) {
   displayDiv.innerHTML = previewPic.alt;
 
   displayDiv.style.backgroundImage = "url('" + previewPic.src + "')";
-
-  console.log("Hình nền hiện tại:", displayDiv.style.backgroundImage);
 }
+
 function unDo() {
   let displayDiv = document.getElementById("image");
 
